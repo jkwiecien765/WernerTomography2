3e66fcaeb297f621d9738d7e4712f72127b3f1cf
jkwiecien765 <146484221+jkwiecien765@users.noreply.github.com>
2023-12-19 14:54:01 +0100

    Another 3000 samples

--- myPackage/__init__.py
+++ myPackage/__init__.py
@@ -1,3 +1,9 @@
 import numpy as np
+import pandas as pd
+from collections.abc import Iterable
+from scipy.linalg import sqrtm
+from numpy import pi
+import matplotlib.pyplot as plt
+
         
 parameters=np.load('parameters.npy')
